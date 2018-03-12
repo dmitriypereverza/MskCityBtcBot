adeb2f3d2f602093ada765e033998f87c0ea9321
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2018-03-13 00:07:42 +0300

    fix code

--- config.py
+++ config.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-WEBHOOK_HOST = '87.117.63.174'
-WEBHOOK_PORT = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+HEROKU_APP_NAME = 'warm-lowlands-57042.herokuapp.com'
 token = "568880226:AAGX19mmh0jgT2BrQqz2johhzJiGPFc0k80"
-WEBHOOK_URL_PATH = "/%s/" % (token)
 
 CHANNEL_NAME = '@omagat920'
 message_chat_id = 433288067
--- run.py
+++ run.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import os
+
 import telebot
+from flask import Flask, request
+
 import config
 import utils
-import cherrypy
 
 bot = telebot.TeleBot(config.token)
-
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            # Эта функция обеспечивает проверку входящего сообщения
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
-
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def message_handler(request):
@@ -35,24 +23,15 @@ def message_handler(request):
         bot.send_message(request.chat.id, config.answers[request.text],
                          reply_markup=telebot.types.ReplyKeyboardHide())
 
+@server.route("/{}".format(config.token), methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
-# Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
-bot.remove_webhook()
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://{}/{}".format(config.HEROKU_APP_NAME, config.token))
+    return "!", 200
 
-# Ставим заново вебхук
-bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
-                certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
-
-bot.get_webhook_info()
-
-cherrypy.config.update({
-    'server.socket_host': config.WEBHOOK_LISTEN,
-    'server.socket_port': config.WEBHOOK_PORT,
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': config.WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': config.WEBHOOK_SSL_PRIV
-})
-
-
-# Собственно, запуск!
-cherrypy.quickstart(WebhookServer(), config.WEBHOOK_URL_PATH, {'/': {}})
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
