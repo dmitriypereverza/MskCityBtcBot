31f4b4ee49c4fecb22fe2a12caf2443663424a0c
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2018-03-13 09:36:40 +0300

    first commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-HEROKU_APP_NAME = 'warm-lowlands-57042.herokuapp.com'
+HEROKU_APP_NAME = 'warm-taiga-86371.herokuapp.com'
 token = "568880226:AAGX19mmh0jgT2BrQqz2johhzJiGPFc0k80"
 
 CHANNEL_NAME = '@omagat920'
