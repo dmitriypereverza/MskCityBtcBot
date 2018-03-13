713665523b6848593d25271fb23c7e0c12838dc7
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2018-03-13 21:41:31 +0300

    Настройка конечного автомата для бота

--- run.py
+++ run.py
@@ -2,28 +2,40 @@
 # -*- coding: utf-8 -*-
 import os
 
+import re
 import telebot
 from flask import Flask, request
 
 import config
 import utils
+from DB import db
 
-bot = telebot.TeleBot(os.environ.get('TELEGRAM_TOKEN'))
+# token = os.environ.get('TELEGRAM_TOKEN')
+token = config.TELEGRAM_TOKEN
+
+bot = telebot.TeleBot(token)
 server = Flask(__name__)
+bot.remove_webhook()
+
+def getTransitionByText(chat_id, text):
+    stateSetting = config.BOT_STATES[db.get_current_state(chat_id)]
+    for transition in stateSetting['transitions']:
+        if re.match(transition['input'], text):
+            return transition
 
 @bot.message_handler(commands=['start'])
 def message_handler(request):
-    answerButtons = ['Купить bitcoin', 'Продать bitcoin', 'Связаться с менеджером']
-    bot.send_message(request.chat.id, 'Что бы вы хотели?', reply_markup=utils.generate_markup(answerButtons))
-    return
+    db.set_state(request.chat.id, config.States.S_START.value)
+    main_message_handler(request)
 
 @bot.message_handler()
-def message_handler(request):
-    if request.text in config.answers.keys():
-        bot.send_message(request.chat.id, config.answers[request.text],
-                         reply_markup=telebot.types.ReplyKeyboardRemove())
+def main_message_handler(request):
+    transition = getTransitionByText(request.chat.id, request.text)
+    if transition:
+        bot.send_message(request.chat.id, transition['text'], reply_markup=utils.generate_markup(transition['keyboard_btn']))
+        db.set_state(request.chat.id, transition['next'])
 
-@server.route("/{}".format(os.environ.get('TELEGRAM_TOKEN')), methods=['POST'])
+@server.route("/{}".format(token), methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
@@ -31,8 +43,11 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://{}/{}".format(config.HEROKU_APP_NAME, os.environ.get('TELEGRAM_TOKEN')))
+    bot.set_webhook(url="https://{}/{}".format(config.HEROKU_APP_NAME, token))
     return "!", 200
 
 if __name__ == '__main__':
-    server.run()
+    if config.env == 'dev':
+        bot.polling(none_stop=True)
+    else:
+        server.run()
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@
 import telebot
 
 def generate_markup(answers):
+    if len(answers) == 0:
+        return telebot.types.ReplyKeyboardRemove()
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     for item in answers:
         markup.add(item)
